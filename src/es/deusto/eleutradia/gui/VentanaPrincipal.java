0be2ae44d36c7d3737d34bac34489f0ee672e086
edurvm <edur.valle@opendeusto.es>
2025-10-29 18:16:20 +0100

    cambios

--- src/es/deusto/eleutradia/gui/VentanaPrincipal.java
+++ src/es/deusto/eleutradia/gui/VentanaPrincipal.java
@@ -1,9 +1,14 @@
 package es.deusto.eleutradia.gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import es.deusto.eleutradia.domain.Usuario;
@@ -13,6 +18,7 @@ public class VentanaPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JLabel labelBienvenida;
+	private JButton botonDashboard, botonBusqueda, botonPortfolio, botonAprendizaje, BotonPerfil;
 	private Usuario usuarioLogueado;
 	
 	public VentanaPrincipal(Usuario usuario) {
@@ -24,6 +30,24 @@ public class VentanaPrincipal extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
+		this.setLayout(new BorderLayout());
+		
+		JPanel panelNavegacion = new JPanel();
+		panelNavegacion.setLayout(new BoxLayout(panelNavegacion, BoxLayout.Y_AXIS));
+		
+		botonDashboard = new JButton("Dashboard");
+		botonBusqueda = new JButton("Búsqueda");
+		botonPortfolio = new JButton("Portfolio");
+		botonAprendizaje = new JButton("Aprendizaje");
+		BotonPerfil = new JButton("Mi perfil");
+		
+		panelNavegacion.add(botonDashboard);
+		panelNavegacion.add(botonBusqueda);
+		panelNavegacion.add(botonPortfolio);
+		panelNavegacion.add(botonAprendizaje);
+		panelNavegacion.add(BotonPerfil);
+		
+		this.add(panelNavegacion, BorderLayout.WEST);
 		labelBienvenida = new JLabel("Login exitoso. USUARIO: " + usuarioLogueado.getNombre(), SwingConstants.CENTER);
 		labelBienvenida.setFont(new Font("Arial", Font.BOLD, 20));
 		this.add(labelBienvenida);
