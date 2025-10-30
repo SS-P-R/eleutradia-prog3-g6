20aae1159f41faad2e6fce9e73013272c1be1484
edurvm <edur.valle@opendeusto.es>
2025-10-30 11:22:00 +0100

    cambios panel busqueda

--- src/es/deusto/eleutradia/gui/PanelBusqueda.java
+++ src/es/deusto/eleutradia/gui/PanelBusqueda.java
@@ -1,5 +1,31 @@
 package es.deusto.eleutradia.gui;
 
-public class PanelBusqueda {
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class PanelBusqueda extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel labelTituloBusqueda;
+	private JTextField campoBusqueda;
+	private JTable tablaResultados;
+	private DefaultTableModel modeloTablaResultado;
+	
+	public PanelBusqueda() {
+		this.setLayout(new BorderLayout(10, 10));
+		this.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		
+		JPanel panelBusqueda = new JPanel(new BorderLayout(5, 5));
+		
+		
+	}
 
 }
