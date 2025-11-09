b2e6cb517f69478d1863e2987688281eb323cbc9
eledersw <e.sweeney@opendeusto.es>
2025-11-09 22:20:20 +0100

    Clase de gestor de temas para el tema oscuro.

---
+++ src/es/deusto/eleutradia/gui/GestorTema.java
@@ -0,0 +1,93 @@
+package es.deusto.eleutradia.gui;
+
+import java.awt.Color;
+
+public class GestorTema {
+	private static GestorTema instancia;
+	private boolean temaOscuro;
+	
+	//Tema claro
+    public static final Color FONDO_CLARO = new Color(248, 249, 250);
+    public static final Color CARD_CLARO = Color.WHITE;
+    public static final Color BORDE_CLARO = new Color(222, 226, 230);
+    public static final Color TEXTO_PRINCIPAL_CLARO = new Color(33, 37, 41);
+    public static final Color TEXTO_SECUNDARIO = new Color(108, 117, 125);   
+    
+    //Tema ocuro
+    public static final Color FONDO_OSCURO = new Color(33, 37, 41);
+    public static final Color CARD_OSCURO = new Color(52, 58, 64);
+    public static final Color BORDE_OSCURO = new Color(73, 80, 87);
+    public static final Color TEXTO_PRINCIPAL_OSCURO = new Color(248, 249, 250);
+    
+    // Colores comunes (sin cambio entre temas)
+    public static final Color ACENTO = new Color(0, 123, 255);
+    public static final Color EXITO = new Color(40, 167, 69);
+    public static final Color PELIGRO = new Color(220, 53, 69);
+    public static final Color ADVERTENCIA = new Color(255, 193, 7);
+    public static final Color GANANCIA = new Color(0, 153, 76);
+    public static final Color PERDIDA = new Color(220, 53, 69);
+    
+    private GestorTema() {
+    	this.temaOscuro=false;
+    }
+    
+    public static GestorTema getInstancia() {
+    	if(instancia==null) {
+    		instancia=new GestorTema();
+    	}
+    	return instancia;
+    }
+    
+    public boolean getTema() {
+    	return this.temaOscuro;
+    }
+    
+    public void cambiarTema() {
+    	this.temaOscuro=!this.temaOscuro;
+    }
+    
+    public void hacerTemaOscuro() {
+    	this.temaOscuro = true;
+    }
+    
+    public Color getColorFondo() {
+        return temaOscuro ? FONDO_OSCURO : FONDO_CLARO;
+    }
+    
+    public Color getColorCard() {
+        return temaOscuro ? CARD_OSCURO : CARD_CLARO;
+    }
+    
+    public Color getColorBorde() {
+        return temaOscuro ? BORDE_OSCURO : BORDE_CLARO;
+    }
+    
+    public Color getColorTextoPrincipal() {
+        return temaOscuro ? TEXTO_PRINCIPAL_OSCURO : TEXTO_PRINCIPAL_CLARO;
+    }
+    
+    public Color getColorTextoSecundario() {
+        return TEXTO_SECUNDARIO;
+    }
+    
+    public Color getColorAcento() {
+        return ACENTO;
+    }
+    
+    public Color getColorExito() {
+        return EXITO;
+    }
+    
+    public Color getColorPeligro() {
+        return PELIGRO;
+    }
+    
+    public Color getColorGanancia() {
+        return GANANCIA;
+    }
+    
+    public Color getColorPerdida() {
+        return PERDIDA;
+    }
+    
+}
