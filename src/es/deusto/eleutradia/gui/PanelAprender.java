6a51b5ff212d3dca689aac7fee772ba291312cc0
edurvm <edur.valle@opendeusto.es>
2026-01-16 17:57:34 +0100

    cambio panel aprender

--- src/es/deusto/eleutradia/gui/PanelAprender.java
+++ src/es/deusto/eleutradia/gui/PanelAprender.java
@@ -179,7 +179,7 @@ public class PanelAprender extends JPanel {
 			if (ventanaPrincipal != null) {
 				Container vistaOriginalCompleta = ventanaPrincipal.getContentPane();
 				
-				PanelAprenderRecursividad panelRecursividad = new PanelAprenderRecursividad();
+				PanelAprenderRecursividad panelRecursividad = new PanelAprenderRecursividad(usuarioLogeado);
 				
 				panelRecursividad.addAccionVolver(eventoVolver -> {
 					ventanaPrincipal.setContentPane(vistaOriginalCompleta);
